c5bd7603bedf846397e667bcffc60111a1b3c7f3
Eric Feinstein <erichfeinstein@gmail.com>
2018-11-24 15:25:10 -0500

    added some more features using Spotify API

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const getToken = require('./validate');
 //Constants
 const key = getToken();
 let currentArtistId = -1;
+//TEMP. Add a Spotify song URL here to test
+let currentTrackId = '6YX4TgG7dOFC931bICwE3O';
+let topTracks = [];
 
 //Querying
 async function searchArtistOrSong(query, type) {
@@ -19,9 +22,9 @@ async function searchArtistOrSong(query, type) {
     if (type === 'artist') {
       //Add ability to move through artists if top result was not accurate
       currentArtistId = result.data.artists.items[0].id;
-      console.log('Found artist', currentArtistId, 'finding related...');
+
       //TEMP, this function will be called elsewhere
-      findRelatedArtists(currentArtistId);
+      findBestTrack(currentArtistId);
     }
     //Handle result in context of function call
     return result.data;
@@ -38,14 +41,65 @@ async function findRelatedArtists(artistId) {
       headers: { Authorization: `Bearer ${token}` },
     });
 
-    console.log('related artists: ', result.data.artists);
     return result.data;
   } catch (err) {
     console.error(err);
   }
 }
+//Get an artist's top tracks and return the track most similar to the one stored in 'currentTrackId'
+async function findBestTrack(artistId) {
+  const token = (await key).data.access_token;
+  try {
+    const topTracksResult = await axios({
+      method: 'get',
+      url: `https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=US`,
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    //Find top tracks to compare
+    topTracks = topTracksResult.data.tracks.map(function(track) {
+      return {
+        name: track.name,
+        id: track.id,
+      };
+    });
+    const trackScores = topTracks.map(function(track) {
+      return compareTracks(currentTrackId, track.id);
+    });
+    let bestScore = await trackScores[0];
+    let bestScoreIndex = 0;
+    for (let i = 1; i < topTracks.length; i++) {
+      if ((await trackScores[i]) < bestScore) {
+        bestScore = await trackScores[i];
+        bestScoreIndex = i;
+      }
+    }
+    console.log('best song match is', topTracks[bestScoreIndex].name);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-//Compare tracks from related artists
+//Returns a 'score' for track 2's similarity to track 1, lower is better
+async function compareTracks(track1Id, track2Id) {
+  const token = (await key).data.access_token;
+  try {
+    const tracksFeatures = await axios({
+      method: 'get',
+      url: `https://api.spotify.com/v1/audio-features?ids=${track1Id}%2C${track2Id}`,
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const track1 = tracksFeatures.data.audio_features[0];
+    const track2 = tracksFeatures.data.audio_features[1];
+    //Absolute difference between the tempos of the songs
+    const tempoDiff = Math.abs(track1.tempo - track2.tempo);
+    //Energy level is a percentage
+    const energyDiff = Math.abs(track1.energy * 100 - track2.energy * 100);
+    //Add more comparison factors here!
+    return tempoDiff + energyDiff;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-// searchArtistOrSong('periphery', 'artist');
-searchArtistOrSong('meshuggah', 'artist');
+//Enter an artist name here to search it, find it's best songs and compare those songs to the one stored in currentSongId
+searchArtistOrSong('queens of the stone age', 'artist');
